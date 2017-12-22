ebb13599cb538053018e31a7d8a1a136f3a12147
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-12-21 17:53:04 -0800

    Addded JS Script

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Hide the spoiler text
+$('.spoiler span').hide();
+
+// When the button is pressed
+$('.spoiler buton').click(function() {
+    $('.spoiler span').unhide();
+});
+    // Show the spoiler text
+
+    // Hide the "Reveal Spoiler" button
