5579eb1f95d991cb5e08ac3da2abd7fa3607f82d
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-12-21 18:28:29 -0800

    Updated button event with dynamically added elements.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// // When the button is pressed
+$('.spoiler').on('click', 'button', function() {
+    // Show the spoiler text
+    $('.spoiler span').show();
+    // Hide the "Reveal Spoiler" button
+    $('.spoiler button').hide();
+});
+
+
 // Create the "Reveal Spoiler" button
 const $button = $('<button>Reveal Spoiler</button>');
 
@@ -7,11 +16,5 @@ $('.spoiler').append($button);
 // Hide the spoiler text
 $('.spoiler span').hide();
 
-// When the button is pressed
-$('.spoiler button').click(function() {
-    // Show the spoiler text
-    $('.spoiler span').show();
-    // Hide the "Reveal Spoiler" button
-    $('.spoiler button').hide();
-});
+
     
